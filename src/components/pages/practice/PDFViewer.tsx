7e598be95314447b9bfc5f0f588e0dc51463ad7a
Xi Chen <100172638xi@gmail.com>
2025-12-21 08:46:13 +0000

    wait no but it doent work conistnetly across brwsers

--- src/components/pages/practice/PDFViewer.tsx
+++ src/components/pages/practice/PDFViewer.tsx
@@ -7,10 +7,14 @@ interface PDFViewerProps {
 }
 
 export const PDFViewer: React.FC<PDFViewerProps> = ({ url }) => {
+  const googleDocsUrl = `https://docs.google.com/gview?url=${encodeURIComponent(
+    url
+  )}&embedded=true`;
+
   return (
     <div className="h-full w-full bg-muted">
       <iframe
-        src={url}
+        src={googleDocsUrl}
         className="h-full w-full"
         frameBorder="0"
         title={`PDF viewer for ${url}`}
