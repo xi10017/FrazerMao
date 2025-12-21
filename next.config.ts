56a4316c15517e5edeee8ff8e2395b9a6e75e1d1
Xi Chen <100172638xi@gmail.com>
2025-12-21 08:57:24 +0000

    Fix build parse error in practice UI

--- next.config.ts
+++ next.config.ts
@@ -30,6 +30,12 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+   webpack: (config) => {
+    config.externals.push({
+      canvas: 'canvas',
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
