f47a9e3610883c50db8708f0363ef3c5d3df505f
Xi Chen <100172638xi@gmail.com>
2025-12-21 04:52:19 +0000

    Fix React hydration mismatch

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en" className="dark" suppressHydrationWarning>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
